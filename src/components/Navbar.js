e57ac5befc76669166251aa5d42eab532721565f
Walid-Ouldali <ouldali02@hotmail.fr>
2022-07-13 17:53:16 +0200

    Adding CGV and ML

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,12 @@
 import React , {useState} from 'react';
 import './Navbar.css';
 import logo1 from '../images/logo1.png';
-
-
+import mlg from '../pdf/mentionlegales.pdf'
+import cgu from '../pdf/CGU.pdf'
 import {FaBars, FaTimes} from 'react-icons/fa';
-
 const Navbar = () => {
     const [click, setClick] = useState(false)
-
     const handleClick = () => setClick(!click)
-
     return (
     <div className='navbar'>
         <div className='logo'>
@@ -17,8 +14,8 @@ const Navbar = () => {
         </div>
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             
-            <li className='nav-item'><a href='/'>CGU</a></li>
-            <li className='nav-item'><a href='/'>Mentions legales</a></li>
+            <li className='nav-item'><a href={cgu}>CGU</a></li>
+            <li className='nav-item'><a href={mlg}>Mentions legales</a></li>
         </ul>
         <div className='hamburger' onClick={handleClick}>
             {click ? (<FaTimes size={30} style={{ color: '#f8f8f8' }} />) : (<FaBars size={30} style={{ color: '#f8f8f8' }} />)}
